394d2215f8d2e3f6d1828e635800d891eb618c84
Egor18 <egorbr1801@mail.ru>
2018-03-21 22:10:13 +0300

    Add start.sh and stop.sh

---
+++ start.sh
@@ -0,0 +1,2 @@
+sudo nohup python3 start.py > /dev/null 2>&1 &
+echo Verifier started.
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#todo: kill by pids
+sudo pkill -f app.py
+sudo pkill -f mailmon.py
+sudo pkill -f start.py
+echo Verifier stopped.
