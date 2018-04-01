760c49285bbaa32166cbb177e1b858454c29e89c
Egor18 <32983915+Egor18@users.noreply.github.com>
2018-04-01 14:07:08 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-sudo nohup python3 start.py > /dev/null 2>&1 &
-echo Verifier started.
+if [ "$(id -u)" != "0" ]; then
+   echo "You should run verifier with sudo privileges!"
+   exit 1
+fi
+rm -f log.txt
+sudo nohup python3 app.py > /dev/null 2>&1 &
+sudo nohup python3 mailmon.py > /dev/null 2>&1 &
+echo "Verifier started."
